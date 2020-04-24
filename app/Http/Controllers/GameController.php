4c8d2366d36139e271018905dd2fb951a8a0fac6
limecodes <yan.shcherbakov+limecodes@gmail.com>
2020-04-24 22:26:43 +0300

    Modified controller for games, added MobileNetwork and Visitor models and migrations, added a Visitor feature test.

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -8,9 +8,8 @@ use Illuminate\Support\Facades\Validator;
 
 class GameController extends Controller
 {
-    public function index($name)
+    public function index($name, Request $request)
     {
-
     	// To make the controller return 422
     	// Later, will show a page with an error
     	$validator = Validator::make(['name' => $name], [
@@ -21,11 +20,12 @@ class GameController extends Controller
     		return response($validator->errors(), 422);
     	}
 
-    	// TODO: This should actually be done through a service
+    	// TODO: This should actually be done through a service //
     	$game = Game::where('name', $name);
 
     	if ($game->count() > 0) {
-    		return view('welcome');
+    		// return view('welcome');
+            return response("The name of the game is $name", 200);
     	} else {
     		return response('game does not exist', 404);
     	}
