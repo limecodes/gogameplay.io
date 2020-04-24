baeb02fd3c1d230c4e886d0b28bde92eb4cc9cb6
limecodes <yan.shcherbakov+limecodes@gmail.com>
2020-04-24 18:42:06 +0300

    Added GameController and added route for game.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect()->away(env('APP_REDIRECT_HOME'));
 });
+
+Route::get('/game/{game}', 'GameController@index');
