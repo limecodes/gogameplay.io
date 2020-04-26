c40cdf8b1934d7e9705c4af4f9591364386b16f1
limecodes <yan.shcherbakov+limecodes@gmail.com>
2020-04-26 12:35:12 +0300

    Moving device detection logic to middleware.

---
+++ app/Http/Middleware/CheckMobileDevice.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckMobileDevice
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $userAgent = $request->server->get('HTTP_USER_AGENT');
+        $iPhone = stripos($userAgent, 'iPhone');
+        $android = stripos($userAgent, 'Android');
+
+        if ($iPhone) {
+            $request->headers->add('DEVICE', 'ios');
+        } else if ($android) {
+            $request->headers->add('DEVICE', 'android');
+        } else {
+            // Redirect this to route showing non-mobile offers
+            $request->headers->add('DEVICE', 'non-mobile');
+        }
+
+        return $next($request);
+    }
+}
