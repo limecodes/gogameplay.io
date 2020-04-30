4d0ff04c7e4f50fd8c3925992827900d49256ada
limecodes <yan.shcherbakov+limecodes@gmail.com>
2020-04-30 16:44:53 +0300

    Implemented updating carrier in controller, all tests for updating carrier pass and updated ConnectionResource so that is doesn't do an unnecessary check for 'unknown' carrier.

--- app/Http/Controllers/CarrierController.php
+++ app/Http/Controllers/CarrierController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\CarrierRequest;
+use App\Http\Resources\ConnectionResource;
+use App\Models\Visitor;
 
 class CarrierController extends Controller
 {
-    //
+    public function updateCarrier(CarrierRequest $request)
+    {
+    	$carrierRequestValidated = $request->validated();
+
+    	$visitor = Visitor::where('uid', $carrierRequestValidated['uid'])->first();
+
+    	$visitor->carrier_from_data = $carrierRequestValidated['carrier'];
+
+    	$visitor->save();
+
+    	return response()->json(new ConnectionResource($visitor), 200);
+    }
 }
--- tests/Feature/CarrierTest.php
+++ tests/Feature/CarrierTest.php
@@ -4,19 +4,111 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
 use Tests\TestCase;
+use App\Models\Visitor;
+use App\Models\Country;
 
 class CarrierTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /**
+     *
+     * @test
+     */
+    public function shouldFailIfUUIDNotSpecified()
+    {
+        $response = $this->json('PATCH', '/api/carrier/update', []);
+
+        $response->assertStatus(422);
+    }
+
+    /**
+     *
+     * @test
+     */
+    public function shouldFailIfCarrierNotSpecified()
+    {
+        $response = $this->json('PATCH', '/api/carrier/update', [
+            'uid' => (string) Str::uuid()
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    /**
+     *
+     * @test
+     */
+    public function shouldUpdateCarrierAndroid()
+    {
+        $country = factory(Country::class)->create();
+
+        $visitor = factory(Visitor::class)->create([
+            'ip_address' => '1.1.1.9',
+            'device' => 'android',
+            'country_id' => $country->id,
+            'mobile_connection' => true,
+            'carrier_from_data' => null
+        ]);
+
+        $response = $this->json('PATCH', '/api/carrier/update', [
+            'uid' => $visitor->uid,
+            'carrier' => 'A-Mobile'
+        ]);
+
+        $this->assertDatabaseHas('visitors', [
+            'uid' => $visitor->uid,
+            'device' => 'android',
+            'country_id' => $country->id,
+            'mobile_connection' => true,
+            'carrier_from_data' => 'A-Mobile'
+        ]);
+
+        $response
+            ->assertOk()
+            ->assertJson([
+                'connection' => true,
+                'carrier' => 'A-Mobile'
+            ]);
+    }
+
+    /**
+     *
+     * @test
+     */
+    public function shouldUpdateCarrierApple()
+    {
+        $country = factory(Country::class)->create();
+
+        $visitor = factory(Visitor::class)->create([
+            'ip_address' => '1.1.1.9',
+            'device' => 'ios',
+            'country_id' => $country->id,
+            'mobile_connection' => false,
+            'carrier_from_data' => null
+        ]);
+
+        $response = $this->json('PATCH', '/api/carrier/update', [
+            'uid' => $visitor->uid,
+            'carrier' => 'A-Mobile'
+        ]);
+
+        $this->assertDatabaseHas('visitors', [
+            'uid' => $visitor->uid,
+            'ip_address' => '1.1.1.9',
+            'device' => 'ios',
+            'country_id' => $country->id,
+            'mobile_connection' => false,
+            'carrier_from_data' => 'A-Mobile'
+        ]);
+
+        $response
+            ->assertOk()
+            ->assertJson([
+                'connection' => false,
+                'carrier' => 'A-Mobile'
+            ]);
     }
 }
