027f65ffb9b6b59e693f2a6f48a33c46b21c3910
limecodes <yan.shcherbakov+limecodes@gmail.com>
2020-05-01 14:06:07 +0300

    Updated Searching view for a better UI. ALWAYS COMMIT OFTEN I ALMOST LOST MY WORK

--- resources/js/components/Searching.js
+++ resources/js/components/Searching.js
@@ -13,7 +13,12 @@ class Searching extends Component {
 		super(props);
 
 		this.state = {
-			searching: true
+			searching: true,
+			stepOne: false,
+      		stepTwo: false,
+      		stepThree: false,
+      		stepFour: false,
+      		progressWidth: '25%'
 		}
 	}
 
@@ -25,17 +30,52 @@ class Searching extends Component {
 			this.props.fetchOffer(this.props.visitor.uid);
 		}
 
-		setTimeout(function() {
+		setTimeout((self) => {
 			self.setState({
-				searching: false
+				stepOne: true,
+				progressWidth: '50%'
 			});
-		}, 1000);
+		}, 2000, this);
 	}
 
 	componentDidUpdate(prevProps) {
 		if (this.props.visitor.uid !== prevProps.visitor.uid) {
 			this.props.fetchOffer(this.props.visitor.uid);
 		}
+
+		// if ( (this.state.stepOne !== prevProps.stepOne) && (this.state.stepOne) ) {
+		// 	console.log('stepOneUpdated');
+	 //        setTimeout((self) => {
+	 //          self.setState({
+	 //            stepTwo: true,
+	 //            progressWidth: '75%'
+	 //          });
+	 //        }, 2000, this);
+  //     	}
+
+  //     	if ( (this.state.stepTwo !== prevProps.stepTwo) && (this.state.stepTwo) ) {
+  //     		console.log('stepTwoUpdated');
+	 //        setTimeout((self) => {
+	 //          self.setState({
+	 //            stepThree: true,
+	 //            progressWidth: '80%'
+	 //          });
+	 //        }, 2000, this);
+  //     	}
+
+  //     	if ( (this.state.stepThree !== prevProps.stepThree) && (this.state.stepThree) ) {
+  //     		console.log('stepThreeUpdated');
+	 //        setTimeout((self) => {
+	 //          self.setState({
+	 //            stepFour: true,
+	 //            progressWidth: '90%'
+	 //          });
+	 //        }, 2000, this);
+      	}
+	}
+
+	handleOfferClick() {
+		document.location.href = this.props.offer.url;
 	}
 
 	render() {
@@ -43,7 +83,7 @@ class Searching extends Component {
 			if (this.state.searching) {
 				return (
 					<div className="progress">
-						<div className="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{ width: '100%'}}></div>
+						<div className="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{ width: this.state.progressWidth}}></div>
 					</div>
 				);
 			} else {
@@ -51,12 +91,66 @@ class Searching extends Component {
 			}
 		};
 
+		const StepOne = () => {
+			if (!this.state.stepOne) {
+				return (<span>Searching Step One...</span>);
+			} else {
+				return (<span><FontAwesomeIcon icon='check' />{' '}Done Step One</span>);
+			}
+		}
+
+		const StepTwo = () => {
+			if (!this.state.stepTwo) {
+				return (<span>Searching Step Two...</span>);
+			} else {
+				return (<span><FontAwesomeIcon icon='check' />{' '}Done Step Two</span>);
+			}
+		}
+
+		const StepThree = () => {
+			if (!this.state.stepThree) {
+				return (<span>Searching Step Three...</span>);
+			} else {
+				return (<span><FontAwesomeIcon icon='check' />{' '}Done Step Three</span>);
+			}
+		}
+
+		const StepFour = () => {
+			if (!this.state.stepFour) {
+				return (<span>Searching Step Four...</span>);
+			} else {
+				return (<span><FontAwesomeIcon icon='check' />{' '}Done Step Four</span>);
+			}
+		}
+
 		return (
 			<div className="card" style={{ marginTop: '1rem' }}>
-				<div className="card-body">
-					{ (!this.state.searching) ? <div><span className="badge badge-success"><FontAwesomeIcon icon='check' /></span>{' '}Coupon Found</div> : <div>Searching</div> }
+				<div className="card-body"> 
+					{/* { (!this.state.searching) ? <div><span className="badge badge-success"><FontAwesomeIcon icon='check' /></span>{' '}Coupon Found</div> : <div>Searching</div> } */}
+					<ul style={{ textAlign: 'left', listStyleType: 'none' }}>
+						<li><StepOne /></li>
+						{ (this.state.stepOne) ? <li><StepTwo /></li> : null }
+						{ ( (this.state.stepOne) && (this.state.stepTwo) ) ? <li><StepThree /></li> : null }
+						{ ( (this.state.stepOne) && (this.state.stepTwo) && (this.state.stepThree) ) ? <li><StepFour /></li> : null }
+					</ul>
 					<Progress />
 					{ ( (this.props.offer.url) && (!this.state.searching) ) ? <a href={ this.props.offer.url } class="btn btn-success">Redeem Coupon</a> : <div></div> }
+				</div>
+				<div className="card-footer">
+					{ 
+						( (this.props.offer.url) && (!this.state.searching) )
+						?
+						<button onClick={ this.handleOfferClick.bind(this) } className='input-group' style={{ width: '100%', padding: 0, 'border': 0, background: 'transparent' }}>
+							<div className='btn btn-outline-success border-right-flat' style={{ width: '80%', background: 'white' }}>Redeem Coupon</div>
+							<div className='input-group-append' style={{ width: '20%' }}>
+								<div className='input-group-text' style={{ width: '100%', paddingLeft: '40%', borderRadius: '0 2.25rem 2.25rem 0', background: '#38C192', border: '1px solid #38C192', color: 'white' }}>
+									<FontAwesomeIcon icon='angle-right' />
+								</div>
+							</div>
+						</button>
+						:
+						<div></div>
+					}
 				</div>
 			</div>
 		);
