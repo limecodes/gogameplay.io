f8215604e71b263753ccc1a4532e50168c0b026e
limecodes <yan.shcherbakov+limecodes@gmail.com>
2020-05-03 13:42:34 +0300

    Using X-Forwarded-For header only on local when using with Charles, no need for setting the API manually through the .env file and also no more need for app redirect in .env file.

--- app/Http/Middleware/RequestIpAddress.php
+++ app/Http/Middleware/RequestIpAddress.php
@@ -16,8 +16,11 @@ class RequestIpAddress
     public function handle($request, Closure $next)
     {
         // I'm not exactly happy doing this way, but it allows me to test locally
-        if (env('APP_ENV') == 'local') {
-            $correctIp = env('APP_TEST_IP');
+        // I'm happier in doing this through Charles, and it will only work when environment is local
+        // Otherwise it'll use the standard REMOTE_ADDR header
+        // To modify the server header, it'll have to be an apache setting
+        if ( (env('APP_ENV') == 'local') && ($request->header('X-Forwarded-For')) ) {
+            $correctIp = $request->header('X-Forwarded-For');
         } else {
             $correctIp = $request->server('REMOTE_ADDR');
         }
