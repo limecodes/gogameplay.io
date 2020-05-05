06cb2e7976f571d5d34527663c1d8c39152b9d98
limecodes <yan.shcherbakov+limecodes@gmail.com>
2020-05-06 00:27:48 +0300

    Updated DeviceHelper for the updated data action.

--- app/Contracts/DeviceHelperInterface.php
+++ app/Contracts/DeviceHelperInterface.php
@@ -8,4 +8,5 @@ interface DeviceHelperInterface {
 	public function getDataAndroid(Visitor $visitor):Visitor;
 	public function getDataApple(Visitor $visitor):Visitor;
 	public function getDataNonMobile(Visitor $visitor):Visitor;
+	public function updatedData(Visitor $visitor):Visitor;
 }
